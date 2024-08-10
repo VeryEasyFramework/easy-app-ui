79aa4593ba0097e3434b10f832d1b98fdf7144fe
eveffer <eliveffer@gmail.com>
2024-08-10 18:20:21 +0300

    add realtime explorer

---
+++ client/src/pages/app/realtime/RealtimePage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Realtime</h1>
+    <!--    <SocketExplorer :api="api" :socket="realtimeClient" />-->
+  </div>
+</template>
+
+<script setup lang="ts">
+
+
+</script>
+
+<style scoped></style>
